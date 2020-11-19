461efd831ecca4a383a63fa34e99793128a9d115
mahvesh869 <74698531+mahvesh869@users.noreply.github.com>
2020-11-19 10:35:33 +0500

    Add files via upload
    1: sidebar or header send kia ha sath ma css files or js files ha 
    2: agy ab ap ny ak page banana ha jaha py sary mobiles show hon or ak side py sidebar ajay or nichy pagination banana dyna ak acha sa page bany 
    3:or side bar ka ak issue bi solve karna ha us ki height auto py ha us ki height fix karni ha

---
+++ sidebar.php
@@ -0,0 +1,247 @@
+
+
+<div class="col-lg-2 ml-5 border  mt-lg-0 mt-4  p-lg-0 d-none d-md-inline-block " style=" background-color: red;">
+					<div class="side-bar p-sm-4 p-3">
+						<div class="search-hotel border-bottom py-2">
+							<h3 class="agileits-sear-head mb-3">Brand</h3>
+							<form action="#" method="post">
+								<input type="search" placeholder="Search Brand..." name="search" required="">
+								<input type="submit" value=" ">
+							</form>
+							<div class="left-side py-2">
+								<ul>
+									<li>
+										<input type="checkbox" class="checked">
+										<span class="span">Samsung</span>
+									</li>
+									<li>
+										<input type="checkbox" class="checked">
+										<span class="span">Red Mi</span>
+									</li>
+									<li>
+										<input type="checkbox" class="checked">
+										<span class="span">Apple</span>
+									</li>
+									<li>
+										<input type="checkbox" class="checked">
+										<span class="span">Nexus</span>
+									</li>
+									<li>
+										<input type="checkbox" class="checked">
+										<span class="span">Motorola</span>
+									</li>
+									<li>
+										<input type="checkbox" class="checked">
+										<span class="span">Micromax</span>
+									</li>
+									<li>
+										<input type="checkbox" class="checked">
+										<span class="span">Lenovo</span>
+									</li>
+									<li>
+										<input type="checkbox" class="checked">
+										<span class="span">Oppo</span>
+									</li>
+									<li>
+										<input type="checkbox" class="checked">
+										<span class="span">Sony</span>
+									</li>
+									<li>
+										<input type="checkbox" class="checked">
+										<span class="span">LG</span>
+									</li>
+									<li>
+										<input type="checkbox" class="checked">
+										<span class="span">One Plus</span>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<!-- ram -->
+						<div class="left-side border-bottom py-2">
+							<h3 class="agileits-sear-head mb-3">Ram</h3>
+							<ul>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">Less than 512 MB</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">512 MB - 1 GB</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">1 GB</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">2 GB</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">3 GB</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">5 GB</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">6 GB</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">6 GB & Above</span>
+								</li>
+							</ul>
+						</div>
+						<!-- //ram -->
+						<!-- price -->
+						<div class="range border-bottom py-2">
+							<h3 class="agileits-sear-head mb-3">Price</h3>
+							<div class="w3l-range">
+								<ul>
+									<li>
+										<input type="checkbox" class="checked">
+										<a href="#">Under 5000</a>
+									</li>
+									<li class="my-1">
+										<input type="checkbox" class="checked">
+										<a href="#">5,000 - 10,000</a>
+									</li>
+									<li>
+										<input type="checkbox" class="checked">
+										<a href="#">10,000 - 15,000</a>
+									</li>
+									<li class="my-1">
+										<input type="checkbox" class="checked">
+										<a href="#">15,000 - 25,000</a>
+									</li>
+									<li>
+										<input type="checkbox" class="checked">
+										<a href="#">25,000 - 35,000</a>
+									</li>
+									<li>
+										<input type="checkbox" class="checked">
+										<a href="#">35,000 - 45,000</a>
+									</li>
+									<li class="mt-1">
+										<input type="checkbox" class="checked">
+										<a href="#">Over 45,000</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<!-- //price -->
+						<!-- discounts -->
+						<div class="left-side border-bottom py-2">
+							<h3 class="agileits-sear-head mb-3">Screen</h3>
+							<ul>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">Less than 5 inches</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">3.0 inch - 4.0 inch</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">4.1 inch - 4.9 inch</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">5.0 inch - 6.9 inch</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">7.0 inch - 8.9 inch</span>
+								</li>
+								
+							</ul>
+						</div>
+						<!-- //discounts -->
+						<!-- offers -->
+						<div class="left-side border-bottom py-2">
+							<h3 class="agileits-sear-head mb-3">Camera</h3>
+							<ul>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">Without camera</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">VGA CAM OR LESS</span>
+								</li>
+								<li>
+										
+									<input type="checkbox" class="checked">
+									<span class="span">1 MP Mobiles</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">2 MP Mobiles</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">3 MP Mobiles</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">5 MP Mobiles</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">8 MP Mobiles</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">13 MP Mobiles</span>
+								</li>
+							
+							</ul>
+						</div>
+						<!-- //offers -->
+						<!-- delivery -->
+						<div class="left-side border-bottom py-2">
+							<h3 class="agileits-sear-head mb-3">Types</h3>
+							<ul>
+							<li>
+									<input type="checkbox" class="checked">
+									<span class="span">Camera Mobiles</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span"> Video Recording</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">Bluetooth Mobiles</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">Dual Sim Phones</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">Wireless LAN</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">MP3 Playback</span>
+								</li>
+								<li>
+									<input type="checkbox" class="checked">
+									<span class="span">FM Radio Mobiles</span>
+								</li>
+									<li>
+									<input type="checkbox" class="checked">
+									<span class="span">Memory Card</span>
+								</li>
+							</ul>
+					
+						</div>
+					
+					</div>
+<div class="bg-dark col-lg-2">oiho</div>
+				</div>
